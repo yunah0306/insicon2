27955cb14fb9ec3de50155822ecefc64bf513954
최윤아 <113593461+yunah0306@users.noreply.github.com>
2023-06-18 02:47:55 +0900

    Update ecollege.py

--- ecollege.py
+++ ecollege.py
@@ -120,7 +120,7 @@ st.header("Ecollege")
 
 img1 = Image.open("안내 사진/영수증픽토그램.jpg")
 img1 = img1.resize((256, 256))
-img2 = Image.open("안내 사진/재활용픽토그램.jpg")
+img2 = Image.open("안내 사진/재활용픽토그램.png")
 img2 = img2.resize((256, 256))
 col1, col2 = st.beta_columns([1, 1])
 with col1:
