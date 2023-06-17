138c155eec702683487cc855a5b82d8dacd92c70
최윤아 <113593461+yunah0306@users.noreply.github.com>
2023-06-18 02:56:07 +0900

    Update ecollege.py

--- ecollege.py
+++ ecollege.py
@@ -123,25 +123,18 @@ img1 = img1.resize((256, 256))
 img2 = Image.open("안내 사진/재활용픽토그램.png")
 img2 = img2.resize((256, 256))
 
-checkbox1 = st.checkbox('영수증 인식하러 가기')
-checkbox2 = st.checkbox('재활용품 분리배출 하러 가기')
-
-if checkbox1:
-    checkbox2 = False
-
-if checkbox2:
-    checkbox1 = False
-
 col1, col2 = st.columns([1, 1])
 with col1:
     st.image(img1, use_column_width=True)
+    checkbox1 = st.checkbox('영수증 인식하러 가기')
     if checkbox1:
-        st.write('영수증 인식하러 가기가 선택되었습니다.')
+        checkbox2 = False
 
 with col2:
     st.image(img2, use_column_width=True)
+    checkbox2 = st.checkbox('재활용품 분리배출 하러 가기')
     if checkbox2:
-        st.write('재활용품 분리배출 하러 가기가 선택되었습니다.')
+        checkbox1 = False
 
 
 
