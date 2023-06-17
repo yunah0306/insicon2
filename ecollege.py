feae36363dee45c054eb94a3cf2b896bd8de9e26
최윤아 <113593461+yunah0306@users.noreply.github.com>
2023-06-18 02:59:39 +0900

    Update ecollege.py

--- ecollege.py
+++ ecollege.py
@@ -127,17 +127,15 @@ col1, col2 = st.columns([1, 1])
 with col1:
     st.image(img1, use_column_width=True)
     checkbox1 = st.checkbox('영수증 인식하러 가기')
-    if checkbox1:
-        checkbox2 = False
-
 with col2:
     st.image(img2, use_column_width=True)
     checkbox2 = st.checkbox('재활용품 분리배출 하러 가기')
-    if checkbox2:
-        checkbox1 = False
-
-
-
+    
+if checkbox1:
+  checkbox2 = False
+  st.write('영수증 인식하러 가볼까요?)
+           
+           
 
 
 
