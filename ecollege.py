f4c354953d350bd5fe1c70da5dad5ea409ad93ff
최윤아 <113593461+yunah0306@users.noreply.github.com>
2023-06-20 02:54:10 +0900

    Update ecollege.py

--- ecollege.py
+++ ecollege.py
@@ -384,4 +384,4 @@ st.sidebar.markdown("""
     </div>
     """.format(st.session_state['point']), unsafe_allow_html=True)
   
-st.text({f'{user_name}님의 적립포인트는 {user_point}입니다')
+st.text(f'{user_name}님의 적립포인트는 {user_point}입니다')
