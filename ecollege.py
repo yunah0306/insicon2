3ed11ef26de4994c284158cbb2cf8b53f15ee033
최윤아 <113593461+yunah0306@users.noreply.github.com>
2023-06-20 02:13:30 +0900

    Update ecollege.py

--- ecollege.py
+++ ecollege.py
@@ -127,7 +127,7 @@ st.sidebar.markdown("""
 if option0 == '회원정보':
   user_name = st.text_input("이름을 입력하세요")
   if user_name:
-    st.sidebar.subheader(f'{user_name}님, Ecollege에 오신걸 환영합니다!')
+    st.header(f'{user_name}님, Ecollege에 오신걸 환영합니다!')
   campus = st.radio('재학중인 학교를 선택하세요', ['서강대학교', '연세대학교' ,'이화여자대학교', '홍익대학교'])
 if option0 == '내 포인트 확인하러 가기':
   st.write(user_point)
